601f7b3b708fb159e2fc9f0c7f5f1b6851f71110
Alini <alini.canedo@gmail.com>
2023-07-21 14:15:29 -0300

    components: cart, games, config: json-server, assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GamesComponent } from './components/store/games/games.component';
+import { CartComponent } from './components/store/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GamesComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
